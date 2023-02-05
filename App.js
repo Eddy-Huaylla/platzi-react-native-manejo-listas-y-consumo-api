683dda4fb859080c5a8248319b937e04635b64fb
Eddy Huaylla Quispe <eddy_huaylla47@hotmail.com>
2023-02-05 18:27:55 -0500

    Configuración de React Navigation

--- App.js
+++ App.js
@@ -1,20 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+
+import { NavigationContainer } from '@react-navigation/native';
+import { NavigationTabs } from './src/navigations/NavigationTabs';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Creación del proyecto: PokeDex</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<NavigationContainer>
+			<NavigationTabs />
+		</NavigationContainer>
+	);
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
