d6031ab313cde7bf6e343674c080dfaf43604033
Eddy Huaylla Quispe <eddy_huaylla47@hotmail.com>
2023-02-05 21:46:00 -0500

    Consumo de APIs con fetch: PokeApi

--- src/screens/Pokedex.js
+++ src/screens/Pokedex.js
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Text } from "react-native";
+
 import { SafeAreaView } from "react-native-safe-area-context";
 
+import { getPokemonsApi } from "../services/pokemon";
+
 export const Pokedex = () => {
+	useEffect(() => {
+		( async () => {
+			await loadPokemons();
+		} )();
+	}, []);
+
+	const loadPokemons = async () => {
+		try {
+			const response = await getPokemonsApi();
+			console.log(response);
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
 	return (
 		<SafeAreaView>
 			<Text>Pokedex</Text>
