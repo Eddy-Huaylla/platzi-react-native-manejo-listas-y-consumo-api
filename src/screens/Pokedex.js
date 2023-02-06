c9ae5e6b53434d94aca405f92cc6b504eaba39b1
Eddy Huaylla Quispe <eddy_huaylla47@hotmail.com>
2023-02-06 15:47:10 -0500

    Información de todos los Pokemon

--- src/screens/Pokedex.js
+++ src/screens/Pokedex.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Text } from "react-native";
 
 import { SafeAreaView } from "react-native-safe-area-context";
@@ -6,6 +6,8 @@ import { SafeAreaView } from "react-native-safe-area-context";
 import { getPokemonsApi } from "../services/pokemon";
 
 export const Pokedex = () => {
+	const [ pokemons, setPokemons ] = useState( [] );
+
 	useEffect(() => {
 		( async () => {
 			await loadPokemons();
@@ -15,7 +17,22 @@ export const Pokedex = () => {
 	const loadPokemons = async () => {
 		try {
 			const response = await getPokemonsApi();
-			console.log(response);
+
+			const pokemonsArray = [];
+			for await ( const pokemon of response.results ) {
+				const pokemonDetails = await getPokemonDetailsByUrlApi( pokemon.url );
+
+				pokemonsArray.push( {
+					id: pokemonDetails.id,
+					name: pokemonDetails.name,
+					type: pokemonDetails.types[0].type.name,
+					order: pokemonDetails.order,
+					imagen: pokemonDetails.sprites.other["official-artwork"].front_default,
+				} );
+			}
+
+			setPokemons([...pokemons, ...pokemonsArray]);
+
 		} catch (error) {
 			console.error(error);
 		}
