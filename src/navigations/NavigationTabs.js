a167389d02041b3a04eacb6469c3428eeb63eec2
Eddy Huaylla Quispe <eddy_huaylla47@hotmail.com>
2023-02-05 21:15:57 -0500

    Diseñando la Tab navigation con iconos

--- src/navigations/NavigationTabs.js
+++ src/navigations/NavigationTabs.js
@@ -1,5 +1,9 @@
 import React from "react";
+import { Image } from "react-native";
+
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import Icon from "react-native-vector-icons/FontAwesome5";
+
 import { Account } from "../screens/Account";
 import { Favorite } from "../screens/Favorite";
 import { Pokedex } from "../screens/Pokedex";
@@ -7,11 +11,48 @@ import { Pokedex } from "../screens/Pokedex";
 const Tab = createBottomTabNavigator();
 
 export const NavigationTabs = () => {
+
+	const renderIcon = ( iconName, color, size ) => {
+		return <Icon name={ iconName } color={color} size={size} />
+	}
+
+	const renderPokeball = () => {
+		return (
+			<Image
+				source={require("../assets/pokeball.webp")}
+				style={{ width: 75, height: 75, top: -15 }}
+			/>
+		);
+	}
+
 	return (
 		<Tab.Navigator>
-			<Tab.Screen name="Favorite" component={ Favorite} />
-			<Tab.Screen name="Pokedex" component={ Pokedex } />
-			<Tab.Screen name="Account" component={ Account } />
+			<Tab.Screen
+				name="Favorite"
+				component={ Favorite}
+				options={{
+					tabBarLabel: "Favoritos",
+					tabBarIcon: ( { color, size } ) => renderIcon( "heart", color, size )
+				}}
+			/>
+
+			<Tab.Screen
+				name="Pokedex"
+				component={ Pokedex }
+				options={{
+					tabBarLabel: "",
+					tabBarIcon: () => renderPokeball()
+				}}
+			/>
+
+			<Tab.Screen
+				name="Account"
+				component={ Account }
+				options={{
+					tabBarLabel: "Mi cuenta",
+					tabBarIcon: ( { color, size }) => renderIcon( "user", color, size )
+				}}
+			/>
 		</Tab.Navigator>
 	)
 }
