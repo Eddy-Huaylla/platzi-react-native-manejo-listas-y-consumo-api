a68ff313993d583a5be5bf5ac231a60ea41f6e15
Eddy Huaylla Quispe <eddy_huaylla47@hotmail.com>
2023-02-06 21:42:36 -0500

    Consultando la API de detalles del Pokemon

--- src/screens/Pokemon.js
+++ src/screens/Pokemon.js
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
+import { getPokemonDetailsApi } from "../services/pokemon";
 
 export const Pokemon = ( props ) => {
-	const { navigation, route } = props;
+	const {
+		navigation,
+		route: { params }
+	} = props;
+	const [ pokemon, setPokemon ] = useState( null );
 
-	console.log(route);
-	console.log(navigation);
+	useEffect( () => {
+		( async () => {
+			try {
+				const response = await getPokemonDetailsApi( params.id );
+				setPokemon(response);
+			} catch (error) {
+				console.error( error );
+				navigation.goBack();
+			}
+		} )();
+	}, [ params ] );
+
+	if ( !pokemon ) return null;
 
 	return (
 		<SafeAreaView>
-			<Text>Pokemon</Text>
+			<Text>Estamos en un POKEMON</Text>
+			<Text>{ pokemon.name }</Text>
 		</SafeAreaView>
 	);
 }
